a6332bb9ef2994ddb9b0996b751305159f3e73ef
Hury <mashurimansur18@gmail.com>
2020-05-22 07:25:00 +0800

    if expression

---
+++ src/IfExpression.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val nilai = 90
+
+    if (nilai > 90) {
+        println("Good Job")
+    } else if (nilai > 75) {
+        println("Not Bad")
+    } else if (nilai > 50) {
+        println("anda hampir gagal")
+    } else {
+        println("coba lagi tahun depan")
+    }
+}
