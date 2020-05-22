b3066a06417797a1c263ece64537380115921f5b
Hury <mashurimansur18@gmail.com>
2020-05-23 01:11:19 +0800

    break continue

---
+++ src/BreakContinue.kt
@@ -0,0 +1,19 @@
+fun main() {
+    //break digunakan untuk menghentikan seluruh perulangan
+    //continue digunakana untuk menghentikan perulangan pada saat kondisi terpenuhi dan kembali meneruskan perulangannya
+    var i = 0
+    while (true) {
+        println("Hello ke $i")
+        i++
+        if (i > 100) {
+            break
+        }
+    }
+
+    for (i in 1..1000){
+        if (i % 2 == 0) {
+            continue
+        }
+        println("Angka $i")
+    }
+}
