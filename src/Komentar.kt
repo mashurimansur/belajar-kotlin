036e6eb92a55212c61d4c77ad9a545aa579a0478
Hury <mashurimansur18@gmail.com>
2020-06-01 22:40:51 +0800

    last update kotlin dasar

---
+++ src/Komentar.kt
@@ -0,0 +1,33 @@
+//komentar terbaik pada kode adalah kode itu sendiri
+//saat membuka kode , kita perlu membuat kode semudah mungkin untuk dibaca
+//namun kadang juga kita butuh menambahkan komentar di kode kotilin kita
+//di kotlin ada 2 jenis komentar
+
+/**
+ * ini adalah multi line komentar
+ */
+
+/**
+ * function ini adalah contoh
+ */
+fun contohFuntion() {
+
+}
+
+/**
+ * ini adlaah function dengan parameter
+ * @param name input nya adalah name
+ */
+fun contohFunctionParameter(name: String) {
+
+}
+
+
+fun main() {
+    // ini untuk komentar single line
+
+    //ini adalah contoh function
+    contohFuntion()
+
+    contohFunctionParameter("Hury") //bisa juga di beri komentar disebelah sini
+}
